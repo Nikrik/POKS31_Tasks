643c958e29ef7a611c124f9c159c77b641c7a908
Nikrik <krikunkrik@gmail.com>
2020-01-19 11:23:30 +0300

    task 5 done

---
+++ Task5/Task5.cpp
@@ -0,0 +1,49 @@
+using namespace std;
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+class Factclass
+{
+private:
+    static long double fact(int x)
+    {
+        long double f=1;
+        for (int i = 1; i < x+1; i++)
+        {
+            f*=i;
+        }
+        return f;
+    }
+
+public:
+    double x;//значение
+    long double calc()
+    {
+        
+        return fact(fact(x));
+    }
+
+    void Input(double x)
+    {
+        this->x=x;
+    }
+
+    Factclass()
+    {
+        x=0;
+    }
+    Factclass(double x)
+    {
+        this->x=x;
+    }
+};
+
+int main()
+{
+    cout<<"Введите число\n";
+    double x;
+    cin>>x;
+    Factclass fact1(x);
+    cout<<fact1.calc();
+}
